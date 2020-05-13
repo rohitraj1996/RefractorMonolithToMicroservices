fe7d3ea6d0db4643bfea594b6cb389e65a6737a1
Rohit Raj <z.rohitraj1996@gmail.com>
2020-05-13 08:19:03 +0530

    Logging database details in user sequelize.ts to check if it is correct inside k8s pod

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -1,7 +1,11 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
-
+console.log(config.database);
+console.log(config.username);
+console.log(config.password);
+console.log(config.host);
+console.log(config.dialect);
 export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
